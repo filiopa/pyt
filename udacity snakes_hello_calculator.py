6ab00c5e2f2b468540a58d5c3e061769b84ef9b6
filiopa <31533599+filiopa@users.noreply.github.com>
2025-02-12 10:22:48 +0200

    Intro to python programming - udacity2
    Snake welcoming, infinite hello(s), and a free calculator!!!! WOW

---
+++ udacity snakes_hello_calculator.py
@@ -0,0 +1,25 @@
+# Serpiente!!!
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
+
+# hello!!!
+num = int(input("Enter an integer: "))
+print("hello " * num)
+
+# Quick math!!! CALCULATOR
+result = eval(input("Enter an expression: "))
+print(result)
